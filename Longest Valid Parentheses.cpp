e3e848b6ea8c2d8bae368fa876de8d1b56233dec
OjjuasGupta <ojjuas4453@gmail.com>
2023-03-18 20:00:01 +0530

    space

--- Longest Valid Parentheses.cpp
+++ Longest Valid Parentheses.cpp
@@ -7,6 +7,7 @@ public:
         vector<int> dp(s2.length());
 
         for (int i = 1; i < s2.length(); ++i)
+        
             if (s2[i] == ')' && s2[i - dp[i - 1] - 1] == '(')
                 dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
 
