72ba5848f8eb1aceea2fad015ddc5c11b47c9eb9
OjjuasGupta <ojjuas4453@gmail.com>
2023-04-24 17:41:57 +0530

    new

--- Trapping Rain water.cpp
+++ Trapping Rain water.cpp
@@ -6,7 +6,7 @@ public:
         const int n = height.size();
         int ans = 0;
         vector<int>l(n); // l[i] := max(height[0..i])
-        vector<int> r(n); // r[i] := max(height[i..n))
+        vector<int>r(n); // r[i] := max(height[i..n))
 
         for (int i = 0; i < n; ++i)
             l[i] = i == 0 ? height[i] : max(height[i], l[i - 1]);
