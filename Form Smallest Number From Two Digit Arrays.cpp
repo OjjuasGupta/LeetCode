303fcae5000859166114a6d89cfb0bb4773e5f6e
OjjuasGupta <ojjuas4453@gmail.com>
2023-04-27 14:23:05 +0530

    new

--- Form Smallest Number From Two Digit Arrays.cpp
+++ Form Smallest Number From Two Digit Arrays.cpp
@@ -3,7 +3,7 @@ class Solution
 public:
     int minNumber(vector<int> &nums1, vector<int> &nums2)
     {
-        int ans = 89; // the largest num we can have
+        int ans = 89; // the largest number we can have
         for (const int a : nums1)
             for (const int b : nums2)
                 ans = min(ans, a == b ? a : min(a, b) * 10 + max(a, b));
