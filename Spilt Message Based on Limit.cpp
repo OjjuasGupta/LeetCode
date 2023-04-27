181b7977877d72925793a754d2aad7d5c42fc5dc
OjjuasGupta <ojjuas4453@gmail.com>
2023-04-27 14:34:56 +0530

    3rdUpdated

--- Spilt Message Based on Limit.cpp
+++ Spilt Message Based on Limit.cpp
@@ -5,15 +5,11 @@ public:
     {
         const int kMessageLength = message.length();
         int b = 1;
-        // Total length of a. Initialized with the length of "1".
+        
         int aLength = sz(1);
 
-        // Total length of b := b * sz(b)
-        // Total length of "</>" := b * 3
         while (b * limit < b * (sz(b) + 3) + aLength + kMessageLength)
         {
-            // If the length the last suffix "<b/b>" := sz(b) * 2 + 3 >= limit,
-            // then it's impossible that the length of "*<b/b>" <= limit.
             if (sz(b) * 2 + 3 >= limit)
                 return {};
             aLength += sz(++b);
@@ -23,7 +19,6 @@ public:
 
         for (int i = 0, a = 1; a <= b; ++a)
         {
-            // Length of "<a/b>" := sz(a) + sz(b) + 3
             const int j = limit - (sz(a) + sz(b) + 3);
             ans.push_back(message.substr(i, j) + "<" + to_string(a) + "/" +
                           to_string(b) + ">");
